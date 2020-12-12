d09648253c1a0fab90acfa7f5d4b879c47911b5a
Patrick Thangarajah <43693196+PatrickThangarajah@users.noreply.github.com>
2020-12-11 19:29:16 -0700

    Usask Questions Added

--- General/Usask Conversion Code/imgtester.py
+++ General/Usask Conversion Code/imgtester.py
@@ -1,41 +1,105 @@
+import re
+import sys
+import codecs
+import xml.etree.ElementTree as ET
+import os
+import shutil
+import random
+from re import sub
+import os
 import shutil
 import glob, os
-import re
+import PIL
+from PIL import Image
 
-import zipfile
-filelocation=r'C:\Users\ptemm\Downloads\Dec 1 Module 2 Questions\GE 124 Question Database - Module 2 b93ac1f22a354b71bb477fc66b46d9f2'
+#import re
+#from re import sub
+#import PIL
+#from PIL import Image
+#subfolders = [ f.path for f in os.scandir(r'C:\Users\ptemm\OneDrive\Testing Module 1 Usask') if f.is_dir() ]
+ 
+filelocation=r'C:\Users\ptemm\Downloads\Module 1 Dec 2\GE 124 Question Database - Module 1 c7df8e2393214b0a9d40fca105b01948\Module 1 Manual Changes'
+newfilelocation=r'C:\Users\ptemm\Downloads\Module 1 Dec 2\GE 124 Question Database - Module 1 c7df8e2393214b0a9d40fca105b01948'
 os.chdir(filelocation)
-filename=[]
-count=1
-problemcount=0;
-problemset=''
-setcount=0
-newimglocation=r'C:\Users\ptemm\Downloads\Dec 1 Module 2 Questions\GE 124 Question Database - Module 2 b93ac1f22a354b71bb477fc66b46d9f2\New Images'
-file2=os.listdir(newimglocation)
-oldimglocation=r'C:\Users\ptemm\Downloads\Dec 1 Module 2 Questions\GE 124 Question Database - Module 2 b93ac1f22a354b71bb477fc66b46d9f2\Past Images'
-print(file2)
-with open('mod2imagetracker.txt', 'w') as f2:
-    for file in glob.glob("*.png"):
-        name=file[:19]
-        for i in range(len(file2)):
-            if name in file2[i] and '-G' not in file2[i]:
-                #moveoldfile
-                oldimgoriginpath=filelocation+"\\"+file
-                oldimgnewpath=oldimglocation+"\\"+file
-                try:
-                    os.rename(oldimgoriginpath,oldimgnewpath)
-                    newimgoriginpath=newimglocation+"\\"+file2[i]
-                    os.rename(newimgoriginpath,oldimgoriginpath)
-                except:
-                    pass
-            else:
-                f2.write(file+'\n')
-#for file in glob.glob("*.png"):
-  #filename=file.replace(' ','')
-  #newname=filelocation+'\\'+filename
-  #newimgname=filename+'.png'
-  #originpath=filelocation+"\\"+file
-  #os.rename(originpath,newname)
+#f1=os.listdir(r'C:\Users\ptemm\Downloads\Dec 1 Module 2 Questions\GE 124 Question Database - Module 2 b93ac1f22a354b71bb477fc66b46d9f2\New Images')
+#f2=os.listdir(r'C:\Users\ptemm\Downloads\Usask Images (2)')
+#copy=0
+#print(f1)
+#print(f2)
+#with open('imglist4.txt', 'w') as f:
+    #for n in range(len(f1)):
+        #copy=0
+        #if f1[n] not in f2:
+            #line=f1[n].replace(' ','-')
+            #f.write(f1[n]+'\n')
+file=r'C:\Users\ptemm\Downloads\Dec 2 Module 3\GE 124 Question Database - Module 3 795ce0a4330f418baa3d485f72bcf63b\STATICS-DSF06-01-01-6eb6489cd59e4183b8f028d3d1274ab0.pg'           
+#for file in glob.glob("*.pg"):
+with open(file,'r') as f:
+    ln = f.readlines()
+    data=''
+    for i, line in enumerate(ln): 
+
+        line=line.replace(r'\(',r'[`')
+        line=line.replace(r'\)',r'`]')
+
+        data=data+line
+       #if '.png' in line: 
+           #m=re.search('\"(.*?)\"',line)
+           #newimgname=m.group(1)
+           #try:
+                  #originpath=newfilelocation+"\\"+newimgname
+                  #with Image.open(originpath) as img:
+                      #width, height = img.size;
+                      #imgscale=1
+                      #if height>500:
+                          #imgscale=500/height
+
+                      #width=width*imgscale
+                      #height=height*imgscale
+                      #line=re.sub('width=>(.*?),',r'width=>'+str(width)+',',line)
+                      #line=re.sub('height=>(.*?)\)',r'height=>'+str(height)+')',line)
+                      #print(line)
+           #except:
+                  #pass  
+    #data=data+line
+with open(file,'w') as f:
+  f.write(data)        
+   
+#count=1
+#problemcount=0;
+#problemset=''
+#setcount=0
+#newimglocation=r'C:\Users\ptemm\Downloads\Dec 1 Module 2 Questions\GE 124 Question Database - Module 2 b93ac1f22a354b71bb477fc66b46d9f2\New Images'
+#file2=os.listdir(newimglocation)
+#oldimglocation=r'C:\Users\ptemm\Downloads\Dec 1 Module 2 Questions\GE 124 Question Database - Module 2 b93ac1f22a354b71bb477fc66b46d9f2\Past Images'
+#print(file2)
+#with open('mod2imagetracker.txt', 'w') as f2:
+    #for file in glob.glob("*.png"):
+        #name=file[:19]
+        #for i in range(len(file2)):
+            #if name in file2[i] and '-G' not in file2[i]:
+                ##moveoldfile
+                #oldimgoriginpath=filelocation+"\\"+file
+                #oldimgnewpath=oldimglocation+"\\"+file
+                #try:
+                    #os.rename(oldimgoriginpath,oldimgnewpath)
+                    #newimgoriginpath=newimglocation+"\\"+file2[i]
+                    #os.rename(newimgoriginpath,oldimgoriginpath)
+                #except:
+                    #pass
+            #else:
+                #f2.write(file+'\n')
+
+for file in glob.glob("*.png"):
+    filename=file.replace(' ','-')
+    newname=filelocation+'\\'+filename
+    newimgname=filename+'.png'
+    originpath=filelocation+"\\"+file
+    try:
+        os.rename(originpath,newname)
+        print(newname)
+    except:
+        pass
 
 
 
---
+++ USask Questions/Module 1/Dec 8 Export-3f72707b-dda5-4c06-a408-2a19b951f03a/GE 124 Question Database - Module 2 9cc673d992ce47c1b4b615581a5f7d90/STATICS-CMC08 03 03 e7cc6b683f9e46e9b91272cac4eba56c.md
@@ -0,0 +1,46 @@
+# STATICS-CMC08.03.03
+
+Automatic Grading: Yes
+Keywords: force and couple system, moment
+Last Edit: Oct 30, 2020 11:22 AM
+Last Edited By: Nicholas Betancourt
+Learning Outcome: Calculate an equivalent force-couple system in 2D given a diagram showing one force or one moment and its perpendicular distance from a point or axis
+Margin of Error: 0
+Module: Module 2: Identify and solve introductory level rigid body statics problems
+Needs Calculator: No
+On Mobius: Yes
+Origin/Author: Zoe
+Question Format: Numerical fill in the blank
+Question Type: A
+Randomizable Parameters: No
+Sean Reviewed: Yes
+Shaobo Reviewed: No
+Sig Fig: Exact
+Status: Approved
+Sub-Outcome: 3. One moment
+Sub-Outcome Code: https://www.notion.so/STATICS-CMC08-03-9e8962177a2d47ce9ecf784762961b68
+Topic/ Unit: 3. Incorporate couples in moment calculations (CMC)
+
+# Question:
+
+Determine the x and y components of the resultant force and the resultant couple moment for the equivalent force couple at point O. Express the sense of the moment with a + or - sign in front of your answer, where + is for CCW and - is for CW.
+
+Resultant force at point O: $\{$____$\hat{i}+$____$\hat{j}\} \text{~N}$
+
+Resultant couple moment at point O: ________ $\text{N}\cdot\text{m}$
+
+![STATICS-CMC08%2003%2003%20e7cc6b683f9e46e9b91272cac4eba56c/Untitled.png](STATICS-CMC08%2003%2003%20e7cc6b683f9e46e9b91272cac4eba56c/Untitled.png)
+
+# Answer:
+
+$0,0$
+
+$+~300$
+
+# Feedback:
+
+A couple moment is a free vector since it will create the same external effects on a body when it is applied at any point $P$ on the body.
+
+In this question, the 300 $\text{N}\cdot\text{m}$ couple moment at $A$ can be moved to $O$ directly.
+
+![STATICS-CMC08%2003%2003%20e7cc6b683f9e46e9b91272cac4eba56c/Untitled%201.png](STATICS-CMC08%2003%2003%20e7cc6b683f9e46e9b91272cac4eba56c/Untitled%201.png)
